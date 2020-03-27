bd2ea8673b237bb8b05916950ff3f27fa90d13f5
Milton Boos Junior <junior_boos@live.com>
2020-03-27 07:50:39 +0000

    Back-End

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello OmniStack ;)
+        </p>
+        <a
+          className="App-link"
+          href="https://open.spotify.com/track/0It6VJoMAare1zdV2wxqZq?si=vL1CFiwRSreF28vyPSlHHQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click here
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
