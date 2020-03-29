22c5c55e4e5cd7a6414755967d9dee6e9235782a
Milton Boos Junior <junior_boos@live.com>
2020-03-29 20:09:03 +0100

    Front-end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './global.css';
+
+import Routes from './routes'
+
+// JSX (JS XML)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello OmniStack ;)
-        </p>
-        <a
-          className="App-link"
-          href="https://open.spotify.com/track/0It6VJoMAare1zdV2wxqZq?si=vL1CFiwRSreF28vyPSlHHQ"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Click here
-        </a>
-      </header>
-    </div>
+    return (
+    <Routes />
   );
 }
 
